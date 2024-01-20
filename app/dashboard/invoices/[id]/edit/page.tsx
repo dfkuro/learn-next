4746e0a6ef657493508989623093d3f33f3169af
dfkuro <izmirreffi@gmail.com>
2024-01-20 13:44:26 -0600

    Chapter 16 completed
    - What is metadata.
    - Types of metadata.
    - How to add an Open Graph image using metadata.
    - How to add a favicon using metadata.

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,6 +2,12 @@ import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchInvoiceById, fetchCustomers } from "@/app/lib/data";
 import { notFound } from "next/navigation";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Edit Invoice",
+  description: "Edit Invoice",
+};
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
@@ -9,8 +15,6 @@ export default async function Page({ params }: { params: { id: string } }) {
     fetchInvoiceById(id),
     fetchCustomers(),
   ]);
-
-  // console.log(invoice, customers);
 
   if (!invoice) {
     notFound();
